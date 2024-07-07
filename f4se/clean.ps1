fe5fe8a8bf03a2b2b03b006e1d50584141405a0d
Peter Carter <11322186+psc-87@users.noreply.github.com>
2024-07-07 16:34:07 -0600

    Delete f4se/clean.ps1

--- f4se/clean.ps1
+++
@@ -1,6 +0,0 @@
-Remove-Item -ErrorAction:SilentlyContinue -Recurse -Force -Path "C:\vs_f4se\common\common\x64_vc11\";
-Remove-Item -ErrorAction:SilentlyContinue -Recurse -Force -Path "C:\vs_f4se\f4se\f4se\x64\";
-Remove-Item -ErrorAction:SilentlyContinue -Recurse -Force -Path "C:\vs_f4se\f4se\f4se_common\x64\";
-Remove-Item -ErrorAction:SilentlyContinue -Recurse -Force -Path "C:\vs_f4se\f4se\placeinred\x64\";
-Remove-Item -ErrorAction:SilentlyContinue -Recurse -Force -Path "C:\vs_f4se\f4se\x64\";
-Remove-Item -ErrorAction:SilentlyContinue -Recurse -Force -Path "C:\vs_f4se\f4se\x64_vc11\";
