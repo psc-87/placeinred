2f443c2c4b5309ce026c0afabb7179e83dcc299c
Peter Carter <petercarter@gmail.com>
2026-06-13 11:30:40 -0600

    Temporarily clear index to force GitHub UI update

--- f4se/f4se/NiTextures.cpp
+++
@@ -1,7 +0,0 @@
-#include "f4se/NiTextures.h"
-
-RelocAddr <_CreateTexture> CreateTexture(0x016C9800);
-
-RelocAddr <_CreateBSShaderTextureSet> CreateBSShaderTextureSet(0x02162680);
-
-RelocAddr <_LoadTextureByPath> LoadTextureByPath(0x0217A9F0);
--- f4se/f4se/ObScript.cpp
+++
@@ -1,6 +0,0 @@
-#include "ObScript.h"
-
-// 
-RelocPtr <ObScriptCommand>	g_firstObScriptCommand(0x02EDF120);
-// 
-RelocPtr <ObScriptCommand>	g_firstConsoleCommand(0x02EEF130);
